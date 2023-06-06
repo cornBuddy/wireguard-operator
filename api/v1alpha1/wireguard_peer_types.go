70503e3b40f2d492f614b4abd83c633b53cb2fa1
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2023-06-06 07:30:27 +0400

    chore(peer): add validation for ip address

--- api/v1alpha1/wireguard_peer_types.go
+++ api/v1alpha1/wireguard_peer_types.go
@@ -11,6 +11,7 @@ type WireguardPeerSpec struct {
 	// Reference to the wireguard resource
 	WireguardRef string `json:"wireguardRef,omitempty"`
 
+	// +kubebuilder:validation:Pattern="((^((([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5]))$)|(^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:))$))"
 	// +kubebuilder:default="192.168.254.2"
 
 	// IP address of the peer
