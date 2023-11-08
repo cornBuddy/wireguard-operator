64b9eb34a004508050ca819fe94a9f9bf98d5196
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2023-11-08 09:17:16 +0400

    feat(spec): check crds existence during smoke tests

--- spec/smoke_test.go
+++ spec/smoke_test.go
@@ -1,10 +1,21 @@
 package spec
 
 import (
+	"context"
+	"os"
+	"path/filepath"
 	"testing"
 	"time"
 
+	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/tools/clientcmd"
+
 	"github.com/stretchr/testify/assert"
+)
+
+var (
+	ctx = context.TODO()
 )
 
 const (
@@ -13,5 +24,37 @@ const (
 )
 
 func TestSamples(t *testing.T) {
-	assert.True(t, true, "it just works")
+	client, err := makeK8sClient()
+	assert.Nil(t, err, "k8s config should be available")
+
+	opts := metav1.GetOptions{}
+	wg, err := client.ApiextensionsV1().CustomResourceDefinitions().
+		Get(ctx, "wireguards.vpn.ahova.com", opts)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, wg, "should have wireguard crd installed")
+
+	peer, err := client.ApiextensionsV1().CustomResourceDefinitions().
+		Get(ctx, "wireguardpeers.vpn.ahova.com", opts)
+	assert.Nil(t, err)
+	assert.NotEmpty(t, peer, "should have peer crd installed")
+}
+
+func makeK8sClient() (*clientset.Clientset, error) {
+	userHomeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	kubeConfigPath := filepath.Join(userHomeDir, ".kube", "config")
+	kubeConfig, err := clientcmd.BuildConfigFromFlags("", kubeConfigPath)
+	if err != nil {
+		return nil, err
+	}
+
+	clientset, err := clientset.NewForConfig(kubeConfig)
+	if err != nil {
+		return nil, err
+	}
+
+	return clientset, nil
 }
